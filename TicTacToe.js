ac33c3ba87f7ea989d4c4e0aa227d12f6d528b3c
Rahul <ietrahul@gmail.com>
2017-07-21 22:39:56 +0530

    create winning line n logic

--- TicTacToe.js
+++ TicTacToe.js
@@ -15,8 +15,10 @@ window.onload=function(){
 function canvasClicked(canvasNumber){
   if (turn%2 == 0){
     draw_x(canvasNumber);
+    checkForWin('X');
   }else {
     draw_o(canvasNumber);
+    checkForWin('O');
   }
   turn++;
 }
@@ -29,10 +31,10 @@ function draw_x(canvasNumber){
     ctx.beginPath();
     ctx.lineWidth = 8;
     ctx.strokeStyle = 'blue';
-    ctx.moveTo(40,30);
-    ctx.lineTo(220,120);
+    ctx.moveTo(40,20);
+    ctx.lineTo(240,120);
     ctx.moveTo(40,120);
-    ctx.lineTo(220,30);
+    ctx.lineTo(240,20);
     ctx.stroke();
     ctx.closePath();
     content[canvasNumber-1] = 'X';
@@ -47,9 +49,53 @@ function draw_o(canvasNumber){
     ctx.strokeStyle = 'black';
     ctx.beginPath();
     ctx.lineWidth = 8;
-    ctx.arc(130,75,60,0,Math.PI*2,true);
+    ctx.arc(150,75,60,0,Math.PI*2,true);
     ctx.stroke();
     ctx.closePath();
     content[canvasNumber-1] = 'O';
   }
 }
+
+function draw_line(canvasNumber,start_x,start_y,end_x,end_y){
+   var theCanvas = "canvas"+canvasNumber;
+    var c = document.getElementById(theCanvas);
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.lineWidth = 12;
+    ctx.strokeStyle = 'red';
+    ctx.moveTo(start_x,start_y);
+    ctx.lineTo(end_x,end_y);
+    //ctx.moveTo(40,120);
+    //ctx.lineTo(220,30);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+
+function checkForWin(symbol){
+  //winningCombinations
+  for ( i = 0; i < winningCombinations.length; i++ ) {
+    if(content[winningCombinations[i][0]] === symbol &&
+       content[winningCombinations[i][1]] === symbol &&
+       content[winningCombinations[i][2]] === symbol){
+       if(i < 3){
+        draw_line(winningCombinations[i][0]+1, 0, 75, 300, 75);
+        draw_line(winningCombinations[i][1]+1, 0, 75, 300, 75);
+        draw_line(winningCombinations[i][2]+1, 0, 75, 300, 75);
+       } else if ( i >=3 && i <6 ){
+        draw_line(winningCombinations[i][0]+1, 130, 0, 130, 200);
+        draw_line(winningCombinations[i][1]+1, 130, 0, 130, 200);
+        draw_line(winningCombinations[i][2]+1, 130, 0, 130, 200);
+       } else if(i == 6) {
+        draw_line(winningCombinations[i][0]+1, 0, 0, 300, 150);
+        draw_line(winningCombinations[i][1]+1, 0, 0, 300, 150);
+        draw_line(winningCombinations[i][2]+1, 0, 0, 300, 150);
+       }else{
+        draw_line(winningCombinations[i][0]+1, 0, 150, 300, 0);
+        draw_line(winningCombinations[i][1]+1, 0, 150, 300, 0);
+        draw_line(winningCombinations[i][2]+1, 0, 150, 300, 0);
+       }
+          setTimeout(function(){ alert("Player "+ symbol + " Won!") }, 100);
+       }
+    }
+}1
