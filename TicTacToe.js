030baedd6c1c182966b91776a29c910a6b768c47
Rahul <ietrahul@gmail.com>
2017-07-29 17:38:42 +0530

    minmax algo

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,39 +1,53 @@
-var content, winningCombinations , turn = 0, choice = "X";
+var content, winningCombinations , turn = 0;  // choice = "X";
+var player = 'o', opponent = 'x';
 //Game methods
 window.onload=function(){
     content = new Array();
     winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     for(var l = 0; l <= 8; l++){
-    content[l]='';
+    content[l]='_';
     }
 }
 
 function myChoice(){
-    choice = document.getElementById("selecto").checked ? 'O' : 'X';
+    opponent = document.getElementById("selecto").checked ? 'o' : 'x';
+    player = (opponent === 'x') ? 'o' : 'x';
+}
+
+function isMovesLeft(content)
+{
+    //for (var i = 0; i < 9; i++)
+      //      if (content[i]=='_')
+   if( content.indexOf("_") > 0) return true;
+   return false;
 }
 
 function canvasClicked(canvasNumber){
   if (turn%2 == 0){
-    choice === "X" ? draw_x(canvasNumber) : draw_o(canvasNumber);
+    opponent === "x" ? draw_x(canvasNumber) : draw_o(canvasNumber);
     //checkForWin('X');
   }else {
-    choice === "X" ? draw_o(canvasNumber) : draw_x(canvasNumber);
+    opponent === "x" ? draw_o(canvasNumber) : draw_x(canvasNumber);
     //checkForWin('O');
   }
 
-  if (turn > 3 && turn < 9){
+var bestMove = findBestMove(content);
+player === "x" ? draw_x(bestMove+1) : draw_o(bestMove+1);
+    //checkForWin('X');
+ /* if (turn > 3 && turn < 9){
     checkForWin(content[canvasNumber-1] );
   } else if (turn >=9) {
     if (checkForWin(content[canvasNumber-1] ) != 0 ) {
       setTimeout(function(){ confirm("THE GAME IS OVER!") }, 100);
       setTimeout(function(){ location.reload(true); }, 100);
     }
-  }
+  }*/
+//
 }
 
 function draw_x(canvasNumber){
    var theCanvas = "canvas"+canvasNumber;
-  if (content[canvasNumber-1] == ''){
+  if (content[canvasNumber-1] == '_'){
     var c = document.getElementById(theCanvas);
     var ctx = c.getContext("2d");
     ctx.beginPath();
@@ -45,7 +59,7 @@ function draw_x(canvasNumber){
     ctx.lineTo(240,25);
     ctx.stroke();
     ctx.closePath();
-    content[canvasNumber-1] = 'X';
+    content[canvasNumber-1] = 'x';
     turn++;
   }
   //if (turn > 3) checkForWin('X');
@@ -53,7 +67,7 @@ function draw_x(canvasNumber){
 
 function draw_o(canvasNumber){
    var theCanvas = "canvas"+canvasNumber;
-  if (content[canvasNumber-1] == ''){
+  if (content[canvasNumber-1] == '_'){
     var c = document.getElementById(theCanvas);
     var ctx = c.getContext("2d");
     ctx.strokeStyle = 'black';
@@ -62,7 +76,7 @@ function draw_o(canvasNumber){
     ctx.arc(150,75,60,0,Math.PI*2,true);
     ctx.stroke();
     ctx.closePath();
-    content[canvasNumber-1] = 'O';
+    content[canvasNumber-1] = 'o';
     turn++;
   }
   //if (turn > 3) checkForWin('X');
@@ -83,8 +97,8 @@ function draw_line(canvasNumber,start_x,start_y,end_x,end_y){
     ctx.closePath();
 }
 
-
-function checkForWin(symbol){
+//evaluate the score win = 10 loss -10 draw 0
+/*function evaluate(symbol){
   //winningCombinations
   for ( i = 0; i < winningCombinations.length; i++ ) {
     if(content[winningCombinations[i][0]] === symbol &&
@@ -107,9 +121,150 @@ function checkForWin(symbol){
         draw_line(winningCombinations[i][1]+1, 0, 150, 300, 0);
         draw_line(winningCombinations[i][2]+1, 0, 150, 300, 0);
        }
-         setTimeout(function(){ confirm("Player "+ symbol + " Won!") }, 100);
-         setTimeout(function(){ location.reload(true); }, 100);
-         return 0;
+         //setTimeout(function(){ confirm("Player "+ symbol + " Won!") }, 100);
+         //setTimeout(function(){ location.reload(true); }, 100);
+         if (player === symbol) return +10;
+         return -10;
        }
     }
+  //for draw
+  return 0;
+}*/
+
+
+function evaluate(b)
+{
+   for ( i = 0; i < winningCombinations.length; i++ ) {
+     if(b[winningCombinations[i][0]] === b[winningCombinations[i][1]] &&
+        b[winningCombinations[i][1]] === b[winningCombinations[i][2]]
+       ) {
+             if (b[winningCombinations[i][0]]==player)
+               return +10;
+             else if (b[winningCombinations[i][0]]==opponent)
+               return -10;
+         }
+   }
+    // Else if none of them have won then return 0
+    return 0;
+}
+
+//minmax
+function minimax(content, depth, isMax)
+{
+
+console.log("Inside minimax");
+    var score = evaluate(content);
+
+    // If Maximizer has won the game return his/her
+    // evaluated score
+    if (score == 10)
+        return score;
+
+    // If Minimizer has won the game return his/her
+    // evaluated score
+    if (score == -10)
+        return score;
+
+    // If there are no more moves and no winner then
+    // it is a tie
+    if (isMovesLeft(content)==false)
+        return 0;
+
+    // If this maximizer's move
+    if (isMax)
+    {
+        var best = -1000;
+
+        // Traverse all cells
+        for (var i = 0; i<9; i++)
+        {
+           // for (var j = 0; j<3; j++)
+           // {
+                // Check if cell is empty
+                if (content[i]=='_')
+                {
+                    // Make the move
+                    content[i] = player;
+
+                    // Call minimax recursively and choose
+                    // the maximum value
+                    best = Math.max( best,
+                        minimax(content, depth+1, !isMax) );
+
+                    // Undo the move
+                    content[i] = '_';
+                }
+            //}
+        }
+        return best;
+    }
+
+    // If this minimizer's move
+    else
+    {
+        var best = 1000;
+
+        // Traverse all cells
+        for (var i = 0; i<9; i++)
+        {
+            //for (var j = 0; j<3; j++)
+            //{
+                // Check if cell is empty
+                if (content[i]=='_')
+                {
+                    // Make the move
+                    content[i] = opponent;
+
+                    // Call minimax recursively and choose
+                    // the minimum value
+                    best = Math.min(best,
+                           minimax(content, depth+1, !isMax));
+
+                    // Undo the move
+                    content[i] = '_';
+                }
+            //}
+        }
+        return best;
+    }
+}
+//
+//call this function
+function findBestMove(content)
+{
+    var bestVal = -1000;
+    var bestMove = -1;
+    // Traverse all cells, evalutae minimax function for
+    // all empty cells. And return the cell with optimal
+    // value.
+    for (var i = 0; i<9; i++)
+    {
+            // Check if celll is empty
+            if (content[i]=='_')
+            {
+                // Make the move
+                content[i] = player;
+
+                // compute evaluation function for this
+                // move.
+                var moveVal = minimax(content, 0, false);
+
+                // Undo the move
+                content[i] = '_';
+
+                // If the value of the current move is
+                // more than the best value, then update
+                // best/
+                if (moveVal > bestVal)
+                {
+                    bestMove = i;
+                    bestVal = moveVal;
+                }
+            }
+    }
+
+    console.log("The value of the best Move is"+  bestVal);
+    console.log("bestMove: "+  bestMove);
+
+    return bestMove;
 }
